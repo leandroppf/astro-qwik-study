52f25cb7eeb10d58df1c77b4a8809ac95975bf6f
Leandro Pedroso <leandroppf@gmail.com>
2023-11-29 17:20:31 -0300

    feat: replaced Card component with Qwik component using tailwindcss

---
+++ src/components/QCard.tsx
@@ -0,0 +1,23 @@
+import { component$ } from "@builder.io/qwik";
+
+interface QCardProps {
+  body: string;
+  href: string;
+  title: string;
+}
+
+const QCard = component$<QCardProps>(({ body, href, title }) => {
+  return (
+    <li class="list-none flex p-px bg-slate-700 rounded-lg shadow-sm hover:bg-gradient-to-r hover:from-violet-500 hover:to-fuchsia-500 transition-all duration-500">
+      <a href={href} class="w-full p-6 rounded-lg text-white opacity-80">
+        <h2 class="m-0 text-xl">
+          {title}
+          <span>&rarr;</span>
+        </h2>
+        <p class="mt-2 mb-0">{body}</p>
+      </a>
+    </li>
+  );
+});
+
+export default QCard;
