a82cc8ce8bb76dc4556b7779b451fd8b1ceb9e0c
Leandro Pedroso <leandroppf@gmail.com>
2023-11-29 22:45:53 -0300

    feat: Added PWA support using @vite-pwa/astro with vite-plugin-pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,41 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import qwikdev from "@qwikdev/astro";
+import AstroPWA from "@vite-pwa/astro";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev(), tailwind()]
+  integrations: [
+    qwikdev(),
+    tailwind(),
+    AstroPWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: {
+        navigateFallback: "/index",
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        globDirectory: "dist",
+      },
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "Edsoma Reading App",
+        short_name: "Edsoma Reading App",
+        description: "Edsoma Reading App for Web",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "favicon.svg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicon.svg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
